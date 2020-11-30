d65865bd59718ec2d3f880c91704c92f75edf748
Gabriel Oliveira <gosilva.pro@gmail.com>
2020-11-30 13:13:06 -0300

    Project created

---
+++ src/main/java/com/trainingspring/estudosistema/EstudosistemaApplication.java
@@ -0,0 +1,13 @@
+package com.trainingspring.estudosistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trainingspring/estudosistema/EstudosistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainingspring.estudosistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

c52a943bf0076d37779e3135c563423cd10a064b
Gabriel Oliveira <gosilva.pro@gmail.com>
2020-12-13 11:57:15 -0300

    Exception Handler findById

--- src/main/java/com/trainingspring/estudosistema/services/UserService.java
+++ src/main/java/com/trainingspring/estudosistema/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.trainingspring.estudosistema.entities.User;
 import com.trainingspring.estudosistema.repositories.UserRepository;
+import com.trainingspring.estudosistema.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
